1d96bea6bac0d0c185757aa0ba1f20e89fabed70
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-08-23 16:40:13 -0300

    Assertivas com AssertJ

--- Spring/primeiroteste/src/test/java/com/cursoteste/PrimeiroTeste.java
+++ Spring/primeiroteste/src/test/java/com/cursoteste/PrimeiroTeste.java
@@ -1,5 +1,7 @@
 package com.cursoteste;
 
+import org.assertj.core.api.Assertions;
+
 //jUnit4
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,6 +21,31 @@ public class PrimeiroTeste {
         Assert.assertEquals(15,resultado);
             //dar erro
         Assert.assertEquals(10,resultado);
+
+    }
+
+
+    @Test
+    public void estruturaDeUmTesteComAssertJ(){
+        //cenario
+        int numero1 = 10, numero2 = 5;
+
+        //execucao
+        int resultado = numero1 + numero2;
+
+        //verificacoes
+            //resultado entre
+        Assertions.assertThat(resultado).isBetween(14,16);
+            //resultado igual
+        Assertions.assertThat(resultado).isEqualTo(15);
+            //resultado maior que numero informado
+        Assertions.assertThat(resultado).isGreaterThan(10);
+            //resultado e possitivo
+        Assertions.assertThat(resultado).isPositive();
+
+
+            //minha string e maior que a passada
+        Assertions.assertThat("").isGreaterThanOrEqualTo("jessica");
 
     }
 }
