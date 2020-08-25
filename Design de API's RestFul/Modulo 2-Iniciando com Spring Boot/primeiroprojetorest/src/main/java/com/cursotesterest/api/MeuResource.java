67bfb79c3508a8e000e8c9fee4dd24c6c7924e32
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-08-25 17:00:25 -0300

    Básico para criação de uma API Rest na prática com Spring boot parte 2

--- Design de API's RestFul/Modulo 2-Iniciando com Spring Boot/primeiroprojetorest/src/main/java/com/cursotesterest/api/MeuResource.java
+++ Design de API's RestFul/Modulo 2-Iniciando com Spring Boot/primeiroprojetorest/src/main/java/com/cursotesterest/api/MeuResource.java
@@ -1,13 +1,43 @@
 package com.cursotesterest.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/clientes")
 public class MeuResource {
 
-//    @RequestMapping(value = "api/clientes/{id}",method = RequestMethod.GET)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public  Cliente salvar(@RequestBody Cliente cliente){
+        //service.save(cliente)
+        return cliente;
+    }
 
-    @GetMapping("api/clientes/{id}")
+//    @PostMapping("/api/clientes")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public ResponseEntity salvar(@RequestBody Cliente cliente){
+//        //service.save(cliente)
+//        return new ResponseEntity(cliente,HttpStatus.CREATED);
+//    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        //service.buscarPorId(id);
+        //service.delete(cliente);
+    }
+
+    @PutMapping("{id}")
+    public Cliente atualizar(@PathVariable Long id,@RequestBody Cliente cliente){
+        //service.buscarPorId(id);
+        //service.update(cliente);
+        return cliente;
+    }
+
+    //@RequestMapping(value = "api/clientes/{id}",method = RequestMethod.GET)
+    @GetMapping("{id}")
     public Cliente obterDadosCliente(@PathVariable Long id){
         System.out.println(String.format("Id recebido via url: %d",id));
         Cliente cliente = new Cliente("Jessica","000.000.000-00");
