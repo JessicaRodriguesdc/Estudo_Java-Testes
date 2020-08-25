af2b6bfa1cd0373201aa6933bb07b773031c5997
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-08-25 18:07:55 -0300

    criando classes de testes para testar Rest Controllers

---
+++ Design de API's RestFul/Modulo 3-Projeto Library API Livros/library-api/src/test/java/com/library/libraryapi/api/resource/BookControllerTest.java
@@ -0,0 +1,32 @@
+package com.library.libraryapi.api.resource;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso.")
+    public void createBookTest(){
+
+    }
+
+    @Test
+    @DisplayName("Deve  lancar erro de validacao quando nao houver dados suficiente para criacao do livro.")
+    public void createInvalidBookTest(){
+
+    }
+}
