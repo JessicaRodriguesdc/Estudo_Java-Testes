c43e69ea28e328b3883e39b832baf1b1571a6ae8
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-08-26 17:08:56 -0300

    Criando a implementação do BookService

--- Design de API's RestFul/Modulo 3-Projeto Library API Livros/library-api/src/test/java/com/library/libraryapi/service/BookServiceTest.java
+++ Design de API's RestFul/Modulo 3-Projeto Library API Livros/library-api/src/test/java/com/library/libraryapi/service/BookServiceTest.java
@@ -1,13 +1,17 @@
 package com.library.libraryapi.service;
 
 import com.library.libraryapi.model.entity.Book;
+import com.library.libraryapi.model.repository.BookRepository;
+import com.library.libraryapi.service.impl.BookServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -16,11 +20,24 @@ public class BookServiceTest {
 
     BookService service;
 
+    @MockBean
+    BookRepository repository;
+
+    @BeforeEach
+    public void setUp(){
+        this.service = new BookServiceImpl(repository);
+    }
+
     @Test
     @DisplayName("Deve salvar um livro")
     public void saveBookTest(){
         //cenario
         Book book = Book.builder().isbn("123").author("Jessi").title("As aventuras").build();
+        Mockito.when( repository.save(book) ).thenReturn(
+                        Book.builder().id(1l)
+                                .isbn("123")
+                                .author("Jessi")
+                                .title("As aventuras").build());
 
         //execulcao
         Book saveBook =  service.save(book);
