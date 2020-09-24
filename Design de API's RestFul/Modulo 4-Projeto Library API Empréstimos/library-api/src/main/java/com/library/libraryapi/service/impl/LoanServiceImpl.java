9912d6c0279934409b54340ddf4434b877c5be79
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-09-23 22:15:32 -0300

    Lançando erro ao tentar fazer empréstimo de um livro já emprestado

--- Design de API's RestFul/Modulo 4-Projeto Library API Empréstimos/library-api/src/main/java/com/library/libraryapi/service/impl/LoanServiceImpl.java
+++ Design de API's RestFul/Modulo 4-Projeto Library API Empréstimos/library-api/src/main/java/com/library/libraryapi/service/impl/LoanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.library.libraryapi.service.impl;
 
+import com.library.libraryapi.exception.BusinessException;
 import com.library.libraryapi.model.entity.Loan;
 import com.library.libraryapi.model.repository.LoanRepository;
 import com.library.libraryapi.service.LoanService;
@@ -14,6 +15,9 @@ public class LoanServiceImpl implements LoanService {
 
     @Override
     public Loan save(Loan loan) {
+        if(repository.existsByBookAndNotReturned(loan.getBook())){
+            throw new BusinessException("Book already loaned");
+        }
         return repository.save(loan);
     }
 }
